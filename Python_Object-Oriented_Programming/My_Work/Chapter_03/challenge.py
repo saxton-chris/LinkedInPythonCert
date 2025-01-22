3173051cee8f21dcc7fb7083524e26f98142329f
saxton-chris <surfde22@gmail.com>
2025-01-22 14:08:08 -0600

    Submitting coding challenge and updates to README.me

--- Python_Object-Oriented_Programming/My_Work/Chapter_03/challenge.py
+++ Python_Object-Oriented_Programming/My_Work/Chapter_03/challenge.py
@@ -23,6 +23,12 @@ class Stock(Asset):
         self.company = company
         self.ticker = ticker
 
+    def __str__(self):
+        return f'{self.ticker}: {self.company} -- ${self.price}'
+    
+    def __lt__(self, value):
+        return self.price < value.price
+
 
 class Bond(Asset):
     def __init__(self, price, description, duration, yieldamt):
@@ -30,6 +36,12 @@ class Bond(Asset):
         self.description = description
         self.duration = duration
         self.yieldamt = yieldamt
+
+    def __str__(self):
+        return f'{self.description}: {self.duration}yr : ${self.price} : {self.yieldamt}%'
+    
+    def __lt__(self, value):
+        return self.yieldamt < value.yieldamt
 
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
